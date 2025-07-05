1ded4603c955ed38953c5a4776d8866126090bfd
0jaaack <jack@hopsoffice.com>
2025-07-05 11:37:37 +0900

    antd-style@^3.7.1 의존성 추가 및 antd 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { StyleProvider } from '@ant-design/cssinjs';
+import { ConfigProvider, type ThemeConfig, Typography } from 'antd';
+
+const { Text } = Typography;
+
+const globalTheme: ThemeConfig = {};
+
 function App() {
-  return <>Business Canvas Assignment</>;
+  return (
+    <StyleProvider>
+      <ConfigProvider theme={globalTheme}>
+        <Text className="text-3xl">Business Canvas Assignment</Text>
+      </ConfigProvider>
+    </StyleProvider>
+  );
 }
 
 export default App;
